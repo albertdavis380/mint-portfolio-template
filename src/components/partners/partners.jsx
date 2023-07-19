27675f81c9ca444dad159ef7ec692326fb5bea6f
albert@webandcrafts.in <albert@webandcrafts.in>
2023-07-19 11:03:18 +0530

    updates...

---
+++ src/components/partners/partners.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Swiper from './swiper';
+
+import "./partners.css";
+
+const partners = () => (
+  <div id="partners">
+    <div className="wrapper">
+      <Swiper/>
+    </div>
+  </div>
+);
+
+export default partners;
